d84f79503cdb81fd129e7ddd71937773e71a45ef
Admin <iggla67@yandex.ru>
2023-03-02 03:23:41 +0400

    4.2

--- src/transport/ServiceStation.java
+++ src/transport/ServiceStation.java
@@ -9,10 +9,11 @@ public class ServiceStation {
     public void addCar(Car car) {
         queue.offer(car);
     }
-
     public void addTruck(Truck truck) {
         queue.offer(truck);
     }
+
+
     public void doService(){
         if(!queue.isEmpty()){
             Transport transport = queue.poll();
